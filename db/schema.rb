401b7d37d8ad807a00feeace84df81aaef4be7f0
Justin Raines <justraines@gmail.com>
2014-04-15 20:11:25 -0400

    switching to postgres for easier heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140415144550) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "to_dos", force: true do |t|
     t.string   "title"
     t.text     "description"
